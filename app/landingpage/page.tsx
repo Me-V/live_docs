71dcf4d6883b1c09ce30ecb839e5eb61f1dc7435
Me-V <gonemanarmy@gmail.com>
2024-08-13 00:28:27 +0530

    fixed it

--- app/landingpage/page.tsx
+++ app/landingpage/page.tsx
@@ -10,7 +10,7 @@ export default function MyComponent() {
   const handleClick = () => {
     router.push('/sign-in');
   };
-  
+
 
   const words = ["Creation", "Editing", "Sharing", "Collaboration"];
 
@@ -23,15 +23,16 @@ export default function MyComponent() {
         baseHue={120}
         className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
       >
-        <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
+        <h2 className="text-white text-5xl md:text-6xl font-bold text-center">
           Lively Docs
         </h2>
         <div className="h-[10rem] flex justify-center items-center px-4">
-          <div className="text-4xl mx-auto font-normal text-white dark:text-neutral-400">
-            <FlipWords words={words} /> 
+          <div className="text-4xl font-normal text-center mx-auto text-white dark:text-neutral-400">
+            <FlipWords words={words} />
             made easy with Lively Docs
           </div>
         </div>
+
         <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
           <button onClick={handleClick} className="p-[3px] relative">
             <div className="absolute inset-0 bg-gradient-to-r from-green-500 to-blue-500 rounded-lg" />
